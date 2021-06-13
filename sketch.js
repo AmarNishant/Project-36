f3671f1b22a2d49063b9345c57104b8a53a92c49
AmarNishant <81952014+AmarNishant@users.noreply.github.com>
2021-06-13 17:42:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,69 +1,165 @@
-//Create variables here
-var dogImage, happydogImage, milkImage;
-var database, foodS, foodStock, feed, lastFed, fedTime, addFood, foodObj;
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var addFood;
+var foodObj;
+
+//create feed and lastFed variable here
+var feed, lastFed;
+var feedDogButton;
+
+var time = 0;
 
 function preload(){
-	//load images here
-  dogImage = loadImage("dogImg.png");
-  happydogImage = loadImage("dogImg1.png");
-  milkImage = loadImage("Milk.png");
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+getTime();
 }
 
 function setup() {
-	createCanvas(900, 500);
+  database=firebase.database();
+  createCanvas(1000,400);
+
   foodObj = new Food();
 
-  dog = createSprite(550,250,30,30);
-  dog.scale=0.3;
-  dog.addImage("dogImg", dogImage);
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
 
-  database = firebase.database();
+  //create feed the dog button here
 
-  foodStock = database.ref('Food');
-  foodStock.on("value", readStock);
-
-  fedTime=database.ref("feedTime");
-  fedTime.on("value",function(data){
-    lastFed=data.val();
-  })
-
-  feed = createButton("feed the dog");
-  feed.position(700,95);
-  feed.mousePressed(feedDog);
-
-  addFood = createButton("add food");
+  addFood=createButton("Add Food");
   addFood.position(800,95);
   addFood.mousePressed(addFoods);
 
+  feedDogButton = createButton("Feed The Dog");
+  feedDogButton.position(700, 95);
+  feedDogButton.mousePressed(feedDog);
+
 }
 
-
-function draw() {  
+function draw() {
   background(46,139,87);
-  fedTime = database.ref('feedTime');
-  fedTime.on("value", function(data){
-    lastFed = data.val();
-    console.log(lastFed);
-  })
-  displayLastFed();
   foodObj.display();
+
+  //write code to read fedtime value from the database 
+  var lastFedRef = database.ref('lastFed');
+  lastFedRef.on("value", function(data){
+    lastFed = data.val();
+  })
+ 
+  //write code to display text lastFed time here
+
+  noStroke();
+  textSize(35);
+  fill('black');
+  if(lastFed === 0){
+    text("Time : 12 PM", 100, 50);
+}
+if(lastFed === 1){
+    text("Time : " + lastFed + " PM", 100, 50);
+}
+if(lastFed === 2){
+    text("Time : " + lastFed + " PM", 100, 50);
+}
+if(lastFed === 3){
+    text("Time : " + lastFed + " PM", 100, 50);
+}
+if(lastFed === 4){
+    text("Time : " + lastFed + " PM", 100, 50);
+}
+if(lastFed === 5){
+    text("Time : " + lastFed + " PM", 100, 50);
+}
+if(lastFed === 6){
+    text("Time : " + lastFed + " PM", 100, 50);
+}
+if(lastFed === 7){
+    text("Time : " + lastFed + " PM", 100, 50);
+}
+if(lastFed === 8){
+    text("Time : " + lastFed + " PM", 100, 50);
+}
+if(lastFed === 9){
+    text("Time : " + lastFed + " PM", 100, 50);
+}
+if(lastFed === 10){
+    text("Time : " + lastFed + " PM", 100, 50);
+}
+if(lastFed === 11){
+    text("Time : " + lastFed + " PM", 100, 50);
+}
+if(lastFed === 12){
+    text("Time : " + lastFed + " AM", 100, 50);
+}
+if(lastFed === -1){
+    text("Time : " + -lastFed + " AM", 100, 50);
+}
+if(lastFed === -2){
+    text("Time : " + -lastFed + " AM", 100, 50);
+}
+if(lastFed === -3){
+    text("Time : " + -lastFed + " AM", 100, 50);
+}
+if(lastFed === -4){
+    text("Time : " + -lastFed + " AM", 100, 50);
+}
+if(lastFed === -5){
+    text("Time : " + -lastFed + " AM", 100, 50);
+}
+if(lastFed === -6){
+    text("Time : " + -lastFed + " AM", 100, 50);
+}
+if(lastFed === -7){
+    text("Time : " + -lastFed + " AM", 100, 50);
+}
+if(lastFed === -8){
+    text("Time : " + -lastFed + " AM", 100, 50);
+}
+if(lastFed === -9){
+    text("Time : " + -lastFed + " AM", 100, 50);
+}
+if(lastFed === -10){
+    text("Time : " + -lastFed + " AM", 100, 50);
+}
+if(lastFed === -11){
+    text("Time : " + -lastFed + " AM", 100, 50);
+}
+
   drawSprites();
 }
 
+//function to read food Stock
 function readStock(data){
-  foodS= data.val();
+  foodS=data.val();
   foodObj.updateFoodStock(foodS);
 }
 
+
 function feedDog(){
-  dog.addImage("dogImg",happydogImage);
-  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  dog.addImage(happyDog);
+
+  //write code here to update food stock and last fed time
+  var food_stock_val = foodObj.getFoodStock();
+  if(food_stock_val <= 0) {
+    foodObj.updateFoodStock(food_stock_val*0)
+  }
+  else {
+    foodObj.updateFoodStock(food_stock_val - 1);
+  }
+
   database.ref('/').update({
-    Food:foodObj.getFoodStock(),
-    feedTime:hour()
+    Food : food_stock_val
+  })
+  
+  database.ref('/').update({
+    lastFed : time
   })
 }
 
+//function to add food in stock
 function addFoods(){
   foodS++;
   database.ref('/').update({
@@ -71,20 +167,13 @@ function addFoods(){
   })
 }
 
-function displayLastFed(){
-  
-  fill(255,255,254);
-  textSize(15);
-  if(lastFed>=12){
-    console.log("inside: "+lastFed);
-    text("Last Feed : "+ lastFed%12 + " PM", 350, 30);
-  }else if(lastFed==0){
-    text("last Feed : 12 AM", 350, 30);
-  }else{
-    text("Last Feed : " +lastFed + " AM", 350, 30);
-  }
+async function getTime(){
+  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = await response.json();
+
+  var datetime = responseJSON.datetime;
+  var hour = datetime.slice(11,13);
+  // console.log(datetime);
+  time = hour - 12;
 }
-
-
-
 
